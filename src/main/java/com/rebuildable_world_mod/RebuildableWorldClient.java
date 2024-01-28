99507a3ab7aafdb4eed5445a40ce975a8e9de685
Shin0kari <kasper13122@mail.ru>
2024-01-28 18:58:15 +0300

    Get started with version  0.0.1-1.20.1

---
+++ src/main/java/com/rebuildable_world_mod/RebuildableWorldClient.java
@@ -0,0 +1,10 @@
+package com.rebuildable_world_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RebuildableWorldClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/rebuildable_world_mod/RebuildableWorldDataGenerator.java
@@ -0,0 +1,11 @@
+package com.rebuildable_world_mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RebuildableWorldDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
